d29c47f55ab7de01626c7ef3df9d3465f38b87c7
Tom <trittson@gmail.com>
2019-12-19 15:45:54 +1000

    First commit

---
+++ offline scripts/process_StaticData.ts
@@ -0,0 +1,84 @@
+// Offline script used to populate the static (i.e. non-custom) data sources
+// Only needs to be run once, and then if Actionstep updates its data in a future release.
+
+// The only argument should be a CSV containing a complete export of AS merge fields.
+// The format must be manually set in column N of the CSV:
+// d = date
+// c = currency
+// s = string
+// n = number (e.g. integer, floating point, percentage)
+// p = phone number
+// b = boolean
+
+const papa = require('papaparse');
+const fs = require('fs');
+
+const csv = process.argv[2];
+const csv_string = fs.readFileSync(csv).toString('utf-8');
+
+let prevDataSource;
+
+const json = {
+    dataSources: []
+};
+
+let dataSources = json.dataSources;
+let currentIndex: number;
+
+papa.parse(csv_string, {
+    complete: function (results){
+
+        console.log('parsed');
+        
+        // Starts at 1 to skip headers
+        for (var i = 1; i < results.data.length; i++) {
+
+            // Skip empty rows
+            if (results.data[i] == "") {
+                continue
+            }
+
+            let source = results.data[i][2];
+            let desc = results.data[i][6];
+            let code = results.data[i][3];
+            let format = results.data[i][13];
+
+            // Manually insert placeholder for Participant Data - System after Participant Data
+            if (prevDataSource == 'Participant Data' && source != prevDataSource) {
+                dataSources.push({
+                    key: 'Participant Data - System',
+                    text: 'Participant Data - System',
+                    fields: []
+                });
+            }
+
+            // Create new datasource if required. This assumes the source file is ordered by data source
+            if (source != prevDataSource) {
+                currentIndex = dataSources.push(
+                    {
+                        key: source,
+                        text: source,
+                        fields: []
+                    }
+                ) - 1;
+            }
+
+            let field = {
+                text: desc,
+                key: code,
+                format: format
+            }
+
+            dataSources[currentIndex].fields.push(field);
+
+            prevDataSource = source;
+            
+        }
+
+        const stringified_json = JSON.stringify(json);
+
+        fs.writeFile('static.json', stringified_json, (err) => {
+            console.log('Success');
+        })
+    }
+})
